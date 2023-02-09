d4f1adb05c1b0ecc9f1843fb22ed3248ab3c6a85
RafaelBlanco95 <rafa.blanco.95@gmail.com>
2023-02-09 17:14:24 +0100

    Bug Fixing

--- webflux-client-micro/src/main/java/com/nttdata/bootcamp/services/PersonService.java
+++ webflux-client-micro/src/main/java/com/nttdata/bootcamp/services/PersonService.java
@@ -9,15 +9,17 @@ import reactor.core.publisher.Flux;
 
 @Service
 public class PersonService {
-	WebClient client1 =WebClient.create("http://localhost:8888/person-list-1");
-	WebClient client2 =WebClient.create("http://localhost:8888/person-list-2");
-	WebClient client3 =WebClient.create("http://localhost:8888/person-list-3");
-	WebClient client4 =WebClient.create("http://localhost:8888/person-list-4");
+	WebClient client1 = WebClient.create("http://localhost:8888/person-list-1");
+	WebClient client2 = WebClient.create("http://localhost:8888/person-list-2");
+	WebClient client3 = WebClient.create("http://localhost:8888/person-list-3");
+	WebClient client4 = WebClient.create("http://localhost:8888/person-list-4");
+
 	public Flux<Persona> allPersons() {
 		
-		Flux<Persona> flux=client1.get().retrieve().bodyToFlux(Persona.class);
-		
-		return flux;
+		return Flux.merge(client1.get().retrieve().bodyToFlux(Persona.class),
+				client2.get().retrieve().bodyToFlux(Persona.class),
+				client3.get().retrieve().bodyToFlux(Persona.class),
+				client4.get().retrieve().bodyToFlux(Persona.class));
 		
 	}
 }
